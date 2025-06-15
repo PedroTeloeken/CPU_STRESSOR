2b8d489e723702402a58455574950a7263bd9d6d
Pedro Teloeken <pedroteloeken2211@gmail.com>
2025-06-15 18:15:31 -0300

    Alterando caminho
    Alterando caminho

--- CpuStressor2/src/main/java/com/mycompany/cpustressor2/CpuStress.java
+++ CpuStressor2/src/main/java/com/mycompany/cpustressor2/CpuStress.java
@@ -4,10 +4,6 @@
 
 package com.mycompany.cpustressor2;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
-
 /**
  *
  * @author pedro
@@ -15,12 +11,8 @@ import java.nio.file.Files;
 
 public class CpuStress {
     static {
-        String currentPath = System.getProperty("user.dir");
-        System.out.println("Caminho atual: " + currentPath);  
-        
-         System.load(currentPath + "\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
-
-        
+        String currentPath = System.getProperty("user.dir");        
+        System.load(currentPath + "\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
         //System.load("C:\\Users\\pedro\\Documents\\NetBeansProjects\\CPU_STRESSOR\\CpuStressor2\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
         //System.load("C:\\Users\\helenas\\Documents\\GitHub\\CPU_STRESSOR\\CpuStressor2\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
         //System.load("C:\\Users\\helenas\\Documents\\GitHub\\CalculadoraC\\c\\libcalculadora.dll");
