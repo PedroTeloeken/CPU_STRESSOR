9d952b5af4c5ef2accc52bac6052af87941ecd23
Helena <helena.slva19@gmail.com>
2025-06-17 14:10:13 -0300

    dll criada dinamicamente
    Tentativa de criar um arquivo jar

--- CpuStressor2/src/main/java/com/mycompany/cpustressor2/CpuStress.java
+++ CpuStressor2/src/main/java/com/mycompany/cpustressor2/CpuStress.java
@@ -4,18 +4,46 @@
 
 package com.mycompany.cpustressor2;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  *
  * @author pedro
  */
 
 public class CpuStress {
-    static {
-        String currentPath = System.getProperty("user.dir");        
-        System.load(currentPath + "\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
-        //System.load("C:\\Users\\pedro\\Documents\\NetBeansProjects\\CPU_STRESSOR\\CpuStressor2\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
-        //System.load("C:\\Users\\helenas\\Documents\\GitHub\\CPU_STRESSOR\\CpuStressor2\\src\\main\\java\\com\\mycompany\\cpustressor2\\C\\cpuStress.dll");
-        //System.load("C:\\Users\\helenas\\Documents\\GitHub\\CalculadoraC\\c\\libcalculadora.dll");
+     static {
+        try {
+            // Lê a DLL do classpath
+            InputStream in = CpuStress.class.getResourceAsStream("/C/CpuStress.dll");
+            if (in == null) {
+                System.out.print("A DLL não foi encontrada no classpath");
+                throw new RuntimeException("DLL não encontrada no classpath.");
+            }
+
+            // Cria um arquivo temporário
+            File tempDll = File.createTempFile("cpuStress", ".dll");
+            tempDll.deleteOnExit();
+
+            // Copia o conteúdo da DLL para o arquivo temporário
+            try (OutputStream out = new FileOutputStream(tempDll)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+
+            // Carrega a DLL
+            System.load(tempDll.getAbsolutePath());
+
+        } catch (IOException | RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 
     public native int stressCores(int[] cores, int duration, int priority);
